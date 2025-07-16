9b8b4a0c672b3f90067bc484ae76ea7d590cea9e
Yuvraj Singh <yuvrajraghuvanshisingh@gmail.com>
2025-07-16 10:19:18 +0530

    final solution of catalans number Tabulation , Recursion , Tabulation

--- Catalan.java
+++ Catalan.java
@@ -10,8 +10,39 @@ public class Catalan {
         }
         return ans;
     }
+
+    public static int catalanTab(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2 ; i<= n ; i++){
+            for(int j = 0 ; j<i ; j++){
+                dp[i] += dp[j] * dp[i-j-1]; // Ci = Cj * Ci-j-1
+            }
+        }
+        return dp[n];
+    }
+
+    public static int catalanMemo(int n ,int dp[]){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int ans = 0;
+        for(int i = 0 ; i<n ; i++){
+            ans += catalanMemo(i , dp) * catalanMemo(n-i-1 , dp);
+        }
+            return dp[n] = ans;
+
+    }
     public static void main(String args[]){
         int n = 4;
-        System.out.print(catlanRec(20));
+
+        // System.out.print(catlanRec(20));
+        int dp[] = new int[n+1];
+        Arrays.fill(dp , -1);
+        System.out.print(catalanTab(40));
     }
 }
