6e58146516d503fc69cba7161dfb9fce8dfe7c52
Yuvraj Singh <yuvrajraghuvanshisingh@gmail.com>
2025-07-18 11:59:47 +0530

    Solve the problem using memoization in O(n2) time complexity

--- chainMultiplicaton.java
+++ chainMultiplicaton.java
@@ -1,7 +1,8 @@
+import java.util.*;
+
 // que--> Matrix chain multiplication
 
 public class chainMultiplicaton{
-
     public static int mcm(int arr[] , int i , int j){
         if(j == i) return 0;
 
@@ -16,9 +17,28 @@ public class chainMultiplicaton{
         return ans;
     }
 
+    public static int mcmMemo(int arr[] , int i , int j , int dp[][]){
+        if(i == j) return 0;
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int k = i ; k<=j-1 ; k++){
+            int cost1 = mcmMemo(arr , i , k ,dp);
+            int cost2 = mcmMemo(arr ,k + 1 , j ,dp);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+            ans = Math.min(ans , cost1+cost2+cost3);
+        }
+        return dp[i][j] = ans;
+    }
+
     public static void main(String args[]){
         int arr[] = {1,2,3,4,3};
         int n = arr.length;
-        System.out.print(mcm(arr , 1 , n-1));
+        int dp[][] = new int[n][n];
+        for(int i =0 ; i<n ; i++){
+        Arrays.fill(dp[i] , -1);
+        }
+        System.out.print(mcmMemo(arr , 1 , n-1 , dp));
     }
 }
